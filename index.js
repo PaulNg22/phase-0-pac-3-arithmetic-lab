2e0011759df9ac44574e9d7761b2d236edb2bee1
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-02 12:59:23 +1000

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+function add (number) {
+    return number+=5;
+}
+
+function subtract(number) {
+    return number-=2;
+}
+
+function multiply(number){
+    return number*=5;
+}
+
+function divide(number){
+    return number/=2;
+}
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function increment(n){
+    return ++n;
+}
+
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n,10);
+}
